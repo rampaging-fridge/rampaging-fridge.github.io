6fe8cde42a2716a90da330d791f604cae58f61c3
Fumiaki Kinoshita <fumiexcel@gmail.com>
2021-05-15 20:16:10 +0900

    play ChunChun Heaven

---
+++ music.js
@@ -0,0 +1,19 @@
+(function(){
+    const context = new AudioContext();
+    const buffer = null;
+    const source = context.createBufferSource();
+    const request = new XMLHttpRequest();
+    request.open('GET', 'chunchun_heaven.mp3', true);
+    request.responseType = 'arraybuffer';
+    request.send();
+    
+    request.onload = function () {
+        var res = request.response;
+        context.decodeAudioData(res, function (buf) {
+        source.buffer = buf;
+        });
+    };
+    source.connect(context.destination);
+    source.start(0);
+    
+})();
